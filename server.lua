dfa19aea798764e2769bf93ec438f497bd9d898a
BrandsPickled <brandonvjcooley@gmail.com>
2019-06-27 13:16:27 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -11,25 +11,25 @@
 --      Edited by BrandsPickled
 --      https://github.com/BrandsPickled/RealisticVehicleFailure/
 
-local CopsConnected       	   = 0
+local MechanicConnected       	   = 0
 
-function CountCops()
+function CountMechanic()
 
     local xPlayers = ESX.GetPlayers()
 
-    CopsConnected = 0
+    MechanicConnected = 0
 
     for i=1, #xPlayers, 1 do
         local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-        if xPlayer.job.name == 'police' then
-            CopsConnected = CopsConnected + 1
+        if xPlayer.job.name == 'mechanic' then
+            MechanicConnected = MechanicConnected + 1
         end
     end
 
-    SetTimeout(120 * 1000, CountCops)
+    SetTimeout(120 * 1000, CountMechanic)
 end
 
-CountCops()
+CountMechanic()
 
 local function checkWhitelist(id)
 	for key, value in pairs(RepairWhitelist) do
@@ -43,8 +43,8 @@ end
 AddEventHandler('chatMessage', function(source, _, message)
 	local msg = string.lower(message)
 	local identifier = GetPlayerIdentifiers(source)[1]
-	if CopsConnected < Config.RequiredRepair.Mechanic then
-		TriggerClientEvent('esx:showNotification', source, _U('act_imp_mechanic') .. Config.RequiredRepair.Mechanic ..  _U('act_imp_mechanic2'))
+	if MechanicConnected > Config.RequiredRepair.Mechanic then
+		TriggerClientEvent('esx:showNotification', source, 'You cant repair your vehicle because there are' .. Config.RequiredRepair.Mechanic ..  'Mechanics on duty!')
 		return
 	end
 	if msg == "/repair" then
