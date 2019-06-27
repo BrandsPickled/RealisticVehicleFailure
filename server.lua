2b89fc52987d6a17444968488bd3c1e22400b147
BrandsPickled <brandonvjcooley@gmail.com>
2019-06-27 13:10:21 +0100

    Added Minimum Mechanic Function to do the command

--- server.lua
+++ server.lua
@@ -8,8 +8,28 @@
 --
 --	https://github.com/iEns/RealisticVehicleFailure
 --
+--      Edited by BrandsPickled
+--      https://github.com/BrandsPickled/RealisticVehicleFailure/
 
+local CopsConnected       	   = 0
 
+function CountCops()
+
+    local xPlayers = ESX.GetPlayers()
+
+    CopsConnected = 0
+
+    for i=1, #xPlayers, 1 do
+        local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+        if xPlayer.job.name == 'police' then
+            CopsConnected = CopsConnected + 1
+        end
+    end
+
+    SetTimeout(120 * 1000, CountCops)
+end
+
+CountCops()
 
 local function checkWhitelist(id)
 	for key, value in pairs(RepairWhitelist) do
@@ -23,6 +43,10 @@ end
 AddEventHandler('chatMessage', function(source, _, message)
 	local msg = string.lower(message)
 	local identifier = GetPlayerIdentifiers(source)[1]
+	if CopsConnected < Config.RequiredRepair.Mechanic then
+		TriggerClientEvent('esx:showNotification', source, _U('act_imp_mechanic') .. Config.RequiredRepair.Mechanic ..  _U('act_imp_mechanic2'))
+		return
+	end
 	if msg == "/repair" then
 		CancelEvent()
 		if RepairEveryoneWhitelisted == true then
