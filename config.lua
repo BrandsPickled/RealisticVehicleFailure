7e2b21149d978a2da940dcdf7ffe176661961d77
BrandsPickled <brandonvjcooley@gmail.com>
2019-06-27 13:12:45 +0100

    Added Config.RequiredRepair

--- config.lua
+++ config.lua
@@ -14,6 +14,12 @@
 
 -- IMPORTANT: Some of these values MUST be defined as a floating point number. ie. 10.0 instead of 10
 
+
+Config.RequiredRepair = {
+	Mechanic = 1
+	}
+
+
 cfg = {
 	deformationMultiplier = -1,					-- How much should the vehicle visually deform from a collision. Range 0.0 to 10.0 Where 0.0 is no deformation and 10.0 is 10x deformation. -1 = Don't touch. Visual damage does not sync well to other players.
 	deformationExponent = 0.4,					-- How much should the handling file deformation setting be compressed toward 1.0. (Make cars more similar). A value of 1=no change. Lower values will compress more, values above 1 it will expand. Dont set to zero or negative.
